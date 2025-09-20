8a2d028af833fdd9a2b4148610762e9617d50728
PRIYA-SIDHU <priyasidhu363@gmail.com>
2025-09-20 09:27:37 +0530

    add logo ,fix featured navlink functionality

--- frontend/truevision/src/components/layouts/dashboard.jsx
+++ frontend/truevision/src/components/layouts/dashboard.jsx
@@ -1,4 +1,5 @@
-
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import Footer from "./footer.jsx";
 import ObjectDetection from "../shared/ObjectDetection.jsx";
 import FaceRecognition from "../shared/facerecognition.jsx";
@@ -9,10 +10,25 @@ import Text from "../shared/text.jsx";
 
 
 const  Dashboard = ()=>{
+    const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash === "#object-detection") {
+      // Wait until dashboard and sections are in DOM
+      setTimeout(() => {
+        const section = document.getElementById("object-detection");
+        if (section) {
+          section.scrollIntoView({ behavior: "auto" });
+        }
+      }, 50); // 100ms is usually good, you can try slightly higher if needed
+    }
+  }, [location]);
 return <>
    <Text></Text>
   
-   <ObjectDetection></ObjectDetection>
+   <div id="object-detection">
+        <ObjectDetection />
+      </div>
    <CurrencyDetector></CurrencyDetector>
    <BookReader></BookReader>
    <FaceRecognition></FaceRecognition>
